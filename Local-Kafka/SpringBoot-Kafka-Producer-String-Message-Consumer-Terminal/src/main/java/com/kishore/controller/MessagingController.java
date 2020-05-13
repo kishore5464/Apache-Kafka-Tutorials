3054a517ee55ef60e3fb288f6dee3fcecfa97f7d
Kishore Kumar <kishorekumar5464@gmail.com>
2020-05-13 10:49:15 +0530

    Message sending in browser and receive to terminal initial commit

---
+++ Local-Kafka/SpringBoot-Kafka-Producer-String-Message-Consumer-Terminal/src/main/java/com/kishore/controller/MessagingController.java
@@ -0,0 +1,26 @@
+package com.kishore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class MessagingController {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public static final String TOPIC = "Kafka_Topic";
+
+	@GetMapping("/send/{data}")
+	public String messaging(@PathVariable("data") String data) {
+
+		kafkaTemplate.send(TOPIC, data);
+
+		return "Message Send Successfully, Data is: " + data;
+	}
+}
