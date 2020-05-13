357d602b0acc10e0372e776d173917f1b8902f2f
Kishore Kumar <kishorekumar5464@gmail.com>
2020-05-13 13:09:45 +0530

    Message sending in browser and receive to terminal as JSON

---
+++ Local-Kafka/SpringBoot-Kafka-Producer-JSON-Message-Consumer-Terminal/src/main/java/com/kishore/SpringBootKafkaProducerJsonMessageConsumerTerminalApplication.java
@@ -0,0 +1,13 @@
+package com.kishore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerJsonMessageConsumerTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProducerJsonMessageConsumerTerminalApplication.class, args);
+	}
+
+}
