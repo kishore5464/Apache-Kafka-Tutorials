70991f18ad5ac0d1008f5526b21c96c06a9f8468
Kishore Kumar <kishorekumar5464@gmail.com>
2020-05-13 14:01:43 +0530

    Done Producer

--- Producer/SpringBoot-Kafka-Producer-JSON-Message-Consumer-Terminal/src/main/java/com/kishore/SpringBootKafkaProducerJsonMessageTerminalApplication.java
+++ Producer/SpringBoot-Kafka-Producer-JSON-Message-Consumer-Terminal/src/main/java/com/kishore/SpringBootKafkaProducerJsonMessageTerminalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootKafkaProducerJsonMessageConsumerTerminalApplication {
+public class SpringBootKafkaProducerJsonMessageTerminalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootKafkaProducerJsonMessageConsumerTerminalApplication.class, args);
+		SpringApplication.run(SpringBootKafkaProducerJsonMessageTerminalApplication.class, args);
 	}
 
 }
