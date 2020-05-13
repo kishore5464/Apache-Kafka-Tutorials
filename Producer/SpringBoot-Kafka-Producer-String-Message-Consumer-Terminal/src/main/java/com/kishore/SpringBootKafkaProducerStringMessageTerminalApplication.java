9433cc2977b3c232c69ee6e86667834d8518887d
Kishore Kumar <kishorekumar5464@gmail.com>
2020-05-13 14:05:52 +0530

    Done Producer

--- Producer/SpringBoot-Kafka-Producer-String-Message-Consumer-Terminal/src/main/java/com/kishore/SpringBootKafkaProducerStringMessageTerminalApplication.java
+++ Producer/SpringBoot-Kafka-Producer-String-Message-Consumer-Terminal/src/main/java/com/kishore/SpringBootKafkaProducerStringMessageTerminalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootKafkaProducerStringMessageConsumerTerminalApplication {
+public class SpringBootKafkaProducerStringMessageTerminalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootKafkaProducerStringMessageConsumerTerminalApplication.class, args);
+		SpringApplication.run(SpringBootKafkaProducerStringMessageTerminalApplication.class, args);
 	}
 
 }
